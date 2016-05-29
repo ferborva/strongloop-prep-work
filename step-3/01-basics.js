9e8a883be46825f3d2ffefa63a76ac7e51c6dfe7
FBV <fbordallo2@gmail.com>
2016-05-29 17:25:05 +0200

    [FBV][Process] - Properties basics

--- step-3/01-basics.js
+++ step-3/01-basics.js
@@ -17,7 +17,7 @@ switch (testToRun) {
 		// As long as we don't shadow it!!!! So don't use process as a variable name. There is always a better option
 		function shadowedProcess () {
 			const process = {
-				label: 'Oh no ... I broke the process \n',
+				label: 'Oh no ... I shadowed the process variable \n',
 				stdout: function () {
 					console.log('Why ...!!!!')
 				}
@@ -33,5 +33,84 @@ switch (testToRun) {
 
 		accessProcess();
 		shadowedProcess();
+		break;
 
+	// General propertie reading
+	case '2':
+
+		// arch - Architecture: 'arm', 'ia32' or 'x64'
+		console.log(`The processor architecture you are running on is: ${process.arch}`);
+
+		// argv - Arguments vector. ¡IMPORTANT!
+		console.log('------------------------');
+		console.log(`First argv element: ${process.argv[0]} (will nearly always be node, if not always)`);
+		console.log(`Second argv element: ${process.argv[1]} (javascript file name being run)`);
+		console.log(`Rest argv element: ${process.argv.slice(2)} (any other parameters passed in)`);
+
+		// config - Object containing the JavaScript representation of the configure options that were used to compile the current Node.js 
+		console.log('------------------------');
+		console.log(`Config object:`);
+		console.log(process.config);
+
+		// connected - If true, messages can be received / sent to and from other processes
+		console.log('------------------------');
+		console.log(`Connection status: ${process.connected}`);
+		
+		// env - Run environment configuration
+		console.log('------------------------');
+		console.log(`Environment (env):`);
+		console.log(process.env);
+
+		// execArgv - Node.js-specific command line options from the executable that started the process.
+		// Example: --harmony		
+		console.log('------------------------');
+		console.log(`Node Specific CLI options passed in on process startup: ${process.execArgv}`);
+
+		// execPath - absolute pathname of the executable that started the process
+		// Run example: node --harmony 01-basics.js 2 bla1 bla2 bla3
+		console.log('------------------------');
+		console.log(`Pathname to executable that started the process: ${process.execPath}`);
+
+		// exitCode - A number which will be the process exit code, when the process either exits gracefully, or is exited via process.exit() without specifying a code
+		console.log('------------------------');
+		console.log(`Configured exit code: ${process.exitCode}`);
+
+		// pid - Process PID
+		console.log('------------------------');
+		console.log(`Process PID: ${process.pid}`);
+		
+		// platform - 'darwin', 'freebsd', 'linux', 'sunos' or 'win32'
+		console.log('------------------------');
+		console.log(`Platform: ${process.platform}`);
+		
+		// release - metadata on current node being run
+		console.log('------------------------');
+		console.log(`Release:`);
+		console.log(process.release);
+
+		// version - compiled-in property that exposes NODE_VERSION
+		console.log('------------------------');
+		console.log(`Version: ${process.version}`);
+
+		// versions - property exposing version strings of Node.js and its dependencies
+		console.log('------------------------');
+		console.log(`Versions:`);
+		console.log(process.versions);
+		break;
+
+	// stdin, stdout and stderr use examples
+	case '3':
+		process.stdin.setEncoding('utf8');
+
+		process.stdin.on('readable', () => {
+		  var chunk = process.stdin.read();
+		  if (chunk !== null) {
+		    process.stdout.write(`stdin data: ${chunk}`);
+		    process.stdin.emit('end');
+		  }
+		});
+
+		process.stdin.on('end', () => {
+		  process.stdout.write('stdin listening ended (emit(\'end\') sent) \n');
+		});
 }
--- step-3/README.md
+++ step-3/README.md
@@ -14,3 +14,34 @@ Given the importance and repeated use of the process global variable a special f
 - On top of all the standard events emitted by any instance of the EventEmitter class, the process offers greater control with process-specific events
 - The process emits special events (SIGNAL EVENTS) when it receives signals from the OS ([list of standard POSIX signal names](http://man7.org/linux/man-pages/man2/sigaction.2.html)) allowing control for your program to react accordingly
 
+#### Properties
+
+All are accessible as would be any other typical object propertie: `process.<prop_name>`. Most used in **bold**:
+
+- arch: processor architecture you are running no ('arm', 'ia32' or 'x64')
+- **argv**: arguments vector. Contains the command line arguments. First element will be 'node', second element will be the name of the javascript file being run and rest any additional arguments passed in
+- config: Object containing the JavaScript representation of the configure options that were used to compile the current Node.js 
+- connected: connection state (Boolean) of the IPC channel. If true, messages can be sent/received to and from other processes
+- **env**: Object containing the user ENVIRONMENT configuration
+- execArgv: Node.js-specific command line options from the executable that started the process. Useful in order to *spawn child processes with the same execution environment as the parent*
+- execPath: absolute pathname of the executable that started the process
+- exitCode: A number which will be the process exit code, when the process either exits gracefully, or is exited via process.exit() without specifying a code. 'undefined' by default
+- mainModule: Alternate way to retrieve require.main. It will be undefined if there was no entry script.
+- pid: Process PID
+- platform: Either 'darwin', 'freebsd', 'linux', 'sunos' or 'win32'
+- release: Object containing metadata related to the current node release being run
+- **stderr**: Writable stream to stderr
+- **stdout**: Writable stream to stdout
+	- *process.stderr and process.stdout are unlike other streams in Node.js in that they cannot be closed (end() will throw), they never emit the finish event and that writes can block when output is redirected to a file*
+- **stdin**: Readable stream to stdin
+- version: compiled-in property that exposes NODE_VERSION
+- versions: property exposing version strings of Node.js and its dependencies
+
+
+###### Properties Basics example file
+
+Run the commands to see the example print out:
+
+1. Basic example of access and shadowing: `node 01-basics.js 1`
+2. Properties printout example: `node --harmony 01-basics.js 2 bla2 bla3 myOhMy`
+3. stdin listening, stdout write and stderr write: `node 01-basics.js 3`
